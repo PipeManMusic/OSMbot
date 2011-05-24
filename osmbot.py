591dbcaede2187a9ac741b806ccb78845854d152
PipeManMusic <PipeManMusic@gmail.com>
2011-05-23 20:27:55 -0500

    Removed midi stuff added more question parsing and a feed parsing command.

--- osmbot.py
+++ osmbot.py
@@ -19,145 +19,46 @@
 #
 #
 
-import string
-from ircbot import SingleServerIRCBot
-import time
-from midiutil.MidiFile import MIDIFile
-from datetime import datetime
 import feedparser
+from ircbot import SingleServerIRCBot
+from irclib import irc_lower
+#import feedparser
 import eliza
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker
+#import question
 
 therapist = eliza.eliza()
-feed = feedparser.parse('http://opensourcemusician.libsyn.com/rss')
-
-ntime = time.time()
-MidiDict = {}
-
-#--- Midi file setup
-osmMidi = MIDIFile(1)
-
-#naming the file with the date and hour
-filename = datetime.now().strftime("%Y%m%d%H%M%S") + ".mid"
-
-
-track = 0
-postime = 0
-nextbeat = 0
-
-#adding name to the track
-osmMidi.addTrackName(track, postime, "OSM BoT MElody")
-#tempo for the midi file to play
-osmMidi.addTempo(track, postime, 110)
-
+#feed = feedparser.parse('http://opensourcemusician.libsyn.com/rss')
 #--- IRC SETUP
 server = "irc.freenode.net"
 port = 6667
 channel = '#opensourcemusicians'
 nickname = 'OSMBot'
 
-#---Importer from file
-t = open('midilist', 'r')
-for i in range(97):
-    linha = t.readline()
-    lido = string.split(linha, " ")
-    nota = string.strip(lido[0])
-    notamidi = string.strip(lido[1])
-    MidiDict[nota] = notamidi
-t.close
 class OsmBot(SingleServerIRCBot):
     def __init__(self, channel, nickname, server, port=6667):
         SingleServerIRCBot.__init__(self, [(server, port)], nickname, nickname)
         self.channel = channel
+    def on_nicknameinuse(self, c, e):
+        c.nick(c.get_nickname() + "_") 
     def on_pubmsg (self, c, e):
-        input = str(e.arguments()[0])
-        name = str(e.source().split ( '!' ) [ 0 ])
-        if input[:1] == "!" and input[:2]!= "!!":
-            note = input[1:]
-            self.command_parser(note, c, name)
-        elif input[:2]=="!!":
-            question = input[2:]
-	    self.eliza_parser(question, c)
-        else:
-            pass
+        a = e.arguments()[0].split(":", 1)
+        if len(a) > 1 and irc_lower(a[0]) == irc_lower(self.connection.get_nickname()):
+            question = irc_lower(a[1].strip())
+            if question == "latest episode":
+                self.getLatestEpisode(c)
+            else:
+                self.eliza_parser(question, c)
     def on_welcome(self, c, e):
         c.join(self.channel)
 
     def eliza_parser(self, question, c):
         c.privmsg(channel, therapist.respond(question))
 
-    def command_parser(self, note, c, name):
-        if note.lower() == "help":
-            c.privmsg( channel, name + ": !Note+index eg. !bb3, !help, !usage, !about, !r - makes a rest, !quit - quits the bot")
-        elif note.lower() == "usage":
-            c.privmsg( channel, name + ''': Usage: to start a command use the '!' char and a note representation in the format like "!C#3" or "!bb2" or !f3. The note index range from C0 to C8. The rest is represented by "!r" ''')
-        elif note.lower() == "midi":
-            print "midi command"
-            midiWriteFile()
-        elif note.lower() == "about":
-            c.privmsg( channel, name + ": The main goal of the bot music game, is to record note entry and rests over time, in a colaborative way, and then export it as a midi file. ")
-        elif note.lower() == "license":
-            c.privmsg(channel, name + ": This Bot is GPL. Code available at http://github.com/PipeManMusic/OSMbot")
-        elif note.lower() == "r":
-            c.privmsg(channel, name + ": rest received")
-            self.midinewrest(c)
-        elif note.lower() == "quit":
-            c.privmsg( channel, name + " :In your dreams, that's only for testing.....")
-        elif note.lower() == "nick":
-            c.privmsg( channel, name + "TEST")
-        elif note.lower() == "python":
-            c.privmsg( channel, name + " :Python is great!!! I am working in it!!!")
-        elif note.lower() == "latest":
-            c.privmsg(channel,  feed.entries[0].title)
-            c.privmsg(channel, feed.entries[0].link)
-        else:
-            self.note_parser(note, c, name)
-
-    def note_parser(self, note, c, name):
-        try:
-            mnote = int(MidiDict[note.lower()])
-            self.midinewnote(mnote, c)
-            c.privmsg(channel, name + " :note received")
-        except KeyError:
-            c.privmsg( channel, name + " :Do you really know what you are doing???")
-            
-    def midinewnote(self, mnote, c):
-        #'writes a midi note and the duration. argument is the midi note number'
-        global nextbeat
-        time1 = noteTime()
-        channel = 0
-        track1 = 0
-        velocity = 120
-        osmMidi.addNote(track1, channel, mnote, nextbeat, time1, velocity)
-#--- midi writer to file
-
-    def midinewrest(self, c):
-        #writes a midi note with Velocity = 0 to make a rest
-        global nextbeat
-        time1 = noteTime()
-        channel = 0
-        track1 = 0
-        velocity = 0
-        mrest = 0
-        osmMidi.addNote(track1, channel, mrest, nextbeat, time1, velocity)
-
-
-
-def midiWriteFile():
-    global filename
-    file = open(str(filename), "wb")
-    osmMidi.writeFile(file)
-    file.close()
-    
-
-
-def noteTime():
-    global nextbeat
-    Ntime = time.time() - ntime
-    Rtime = Ntime / 100
-    nextbeat1 = nextbeat
-    nextbeat = nextbeat1 + Rtime
-    return Rtime
-
+    def getLatestEpisode(self, c):
+        feed = feedparser.parse("http://opensourcemusician.libsyn.com/rss")
+        c.privmsg(channel, feed['items'][0]['links'][1]['href'])
 
 bot = OsmBot(channel, nickname, server, port)
 bot.start()
